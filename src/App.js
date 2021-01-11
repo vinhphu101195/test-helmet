4c34644579115a9c236a320bdb99ce8f8ba4dedd
phucv <phucv@alive.vn>
2021-01-11 17:42:17 +0700

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello{" "}
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Hello test page</title>
+        <meta
+          name="description"
+          content="Vào app để kết bạn trò chuyện ngay nào! Tải về ứng dụng ở CH Play Apple Store EVILA để mở yến tiệc tưng bừng cùng nhiều idol xinh đẹp."
+        />
+        <meta
+          name="keywords"
+          content="evila, show bigo, livestream, nimo tv, trực tiếp, phòng kín, yến tiệc, idol"
+        />
+
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta
+          property="og:image"
+          content="https://static.evi.la/resource/wm/og_evila_default.jpg"
+        />
+        <meta
+          property="og:image:alt"
+          content="evila, idol, livestream, show, sexy, bae"
+        />
+
+        <meta
+          property="og:title"
+          content="Xem livestream trực tiếp tại Evila"
+        />
+        <meta
+          property="og:description"
+          content="Vào app để kết bạn trò chuyện ngay nào! Tải về ứng dụng ở CH Play Apple Store EVILA để mở yến tiệc tưng bừng cùng nhiều idol xinh đẹp."
+        />
+      </Helmet>
     </div>
   );
 }
